b192a34babdcd789381e3e0fd2bf5f040ef1a40d
ugur.kotbas <ugur.kotbas@turksat.com.tr>
2025-11-22 12:39:44 +0300

    Footer TBC servisleri modal içerikleri 'Yakında' olarak güncellendi

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -58,11 +58,9 @@ function Footer() {
               </button>
             </div>
             <div className="modal-body">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-              
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              
-              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+              <p style={{ textAlign: 'center', fontSize: '24px', fontWeight: '600', padding: '40px 20px' }}>
+                Yakında
+              </p>
             </div>
           </div>
         </div>
